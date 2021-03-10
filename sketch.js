d53b4a869a89dbab85dde70aed3b21e36099ddf6
Randomizer3151 <74077019+Randomizer3151@users.noreply.github.com>
2021-03-10 15:17:14 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+// A Basic Cake Recipe
+
+//Ingredients:
+// All-Purpose Flour, Eggs, Sugar, Whipped Cream, essence(Vanilla), milk, oil, baking soda, butter
+
+// Tools
+// Oven, Egg beater, baking tray, large bowl
+
+// Instructions
+// Crack the eggs, then put them in the bowl and add the milk and oil. Mix well with the beater.
+//Then add all-purposse flour, sugar and baking soda to it. Add the vanilla essence. Mix very well before proceding.
+// Take baking tray apply some butter, dust the tray with all-purpose flour. Pour the mixture into the tray.
+// Place the baking tray into the tray and set the temp to 350 degrees F. Keep it in the oven for 30 to 35 minutes.
+// Let cool and add whipped cream to it. Cut into slices and enjoy!!!
